996af2830549b3bff14f30338a6f7765cedf8a45
VG2800 <goelvedant28@gmail.com>
2026-03-30 20:05:24 -0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const repoName = "interactive-cuj-presentation-randomizer-railguard"; // dcsil repo name
 const nextConfig = {
   output: "export",
-  basePath: "/" + repoName,
-  assetPrefix: "/" + repoName + "/",
   typescript: {
     ignoreBuildErrors: true,
   },
